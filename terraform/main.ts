c14ab7d4057a85dae8e16b0ebfb7fbea7ba43800
Denis Artyuhovich <i@denis.by>
2021-02-15 16:27:24 +0000

    add route53 configuration to route traffic to real domain

--- terraform/main.ts
+++ terraform/main.ts
@@ -1,26 +1,47 @@
 import { Construct } from 'constructs';
 import { App, TerraformStack } from 'cdktf';
-import { AwsProvider, S3Bucket } from "./.gen/providers/aws";
+import { AwsProvider, S3Bucket, Route53Zone, Route53Record } from "./.gen/providers/aws";
 
 class MyStack extends TerraformStack {
     constructor(scope: Construct, name: string) {
         super(scope, name);
 
-        const bucketName = 'iac-talk-demo-project.ux.by'
+        const domainHost = `ux.by`
+        const domainName = `iac-talk-demo-project.${domainHost}`
 
         const provider = new AwsProvider(this, 'default', {
             region: 'us-east-1',
         });
 
-        new S3Bucket(this, 'resource', {
-            bucket: bucketName,
+        const bucket = new S3Bucket(this, 'resource', {
             provider,
+            bucket: domainName,
             acl: "public-read",
             website: [
                 {
                     indexDocument: 'index.html'
                 }
             ],
+        });
+
+        const route53Zone = new Route53Zone(this, 'zone', {
+            provider,
+            name: domainHost,
+        });
+
+        new Route53Record(this, 'domain', {
+            provider,
+            zoneId: route53Zone.zoneId,
+            name: domainName,
+            type: 'A',
+
+            alias: [
+                {
+                    evaluateTargetHealth: false,
+                    name: bucket.websiteDomain,
+                    zoneId: bucket.hostedZoneId
+                }
+            ]
         });
     }
 }
