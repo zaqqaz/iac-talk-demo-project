5982c5624d5ef610966941d2ab17c419af1e21f3
Denis Artyuhovich <i@denis.by>
2021-02-15 22:23:48 +0000

    add cloudfront and aws certificate manager to infrastructure

--- terraform/main.ts
+++ terraform/main.ts
@@ -1,6 +1,14 @@
 import { Construct } from 'constructs';
 import { App, TerraformStack } from 'cdktf';
-import { AwsProvider, S3Bucket, Route53Zone, Route53Record } from "./.gen/providers/aws";
+import {
+    AwsProvider,
+    S3Bucket,
+    Route53Zone,
+    Route53Record,
+    CloudfrontDistribution,
+    AcmCertificate,
+    AcmCertificateValidation
+} from "./.gen/providers/aws";
 
 class MyStack extends TerraformStack {
     constructor(scope: Construct, name: string) {
@@ -24,9 +32,77 @@ class MyStack extends TerraformStack {
             ],
         });
 
+        const acmCertificate = new AcmCertificate(this, 'AcmCertificate', {
+            provider,
+            domainName: domainHost,
+            subjectAlternativeNames: [
+                `*.${domainHost}`,
+            ],
+            validationMethod: 'DNS',
+            lifecycle: {
+                createBeforeDestroy: true,
+            }
+        });
+
         const route53Zone = new Route53Zone(this, 'zone', {
             provider,
             name: domainHost,
+        });
+
+        const certificateValidationRecord = new Route53Record(this, 'certificateValidation', {
+            zoneId: route53Zone.zoneId,
+            name: acmCertificate.domainValidationOptions('0').resourceRecordName,
+            type: acmCertificate.domainValidationOptions('0').resourceRecordType,
+            records: [
+                acmCertificate.domainValidationOptions('0').resourceRecordValue
+            ],
+            ttl: 60
+        });
+
+        new AcmCertificateValidation(this, 'AcmCertificateValidation', {
+            provider,
+            certificateArn: acmCertificate.arn,
+            validationRecordFqdns: [certificateValidationRecord.fqdn]
+        });
+
+        const cloudfrontDistribution = new CloudfrontDistribution(this, 'cloudfront', {
+            provider,
+            origin: [{
+                domainName: bucket.bucketRegionalDomainName,
+                originId: domainName,
+            }],
+            enabled: true,
+            defaultRootObject: 'index.html',
+            aliases: [domainName],
+            restrictions: [{
+                geoRestriction: [{
+                    restrictionType: 'none',
+                }]
+            }],
+            defaultCacheBehavior: [
+                {
+                    viewerProtocolPolicy: "redirect-to-https",
+                    compress: true,
+                    allowedMethods: ["GET", "HEAD", "OPTIONS"],
+                    cachedMethods: ["GET", "HEAD", "OPTIONS"],
+                    targetOriginId: domainName,
+                    minTtl: 0,
+                    defaultTtl: 86400,
+                    maxTtl: 31536000,
+
+                    forwardedValues: [{
+                        queryString: false,
+                        cookies: [{
+                            forward: "none"
+                        }]
+                    }]
+                }
+            ],
+            orderedCacheBehavior: [],
+            viewerCertificate: [{
+                acmCertificateArn: acmCertificate.arn,
+                sslSupportMethod: "sni-only",
+            }]
         });
 
         new Route53Record(this, 'domain', {
@@ -38,8 +114,8 @@ class MyStack extends TerraformStack {
             alias: [
                 {
                     evaluateTargetHealth: false,
-                    name: bucket.websiteDomain,
-                    zoneId: bucket.hostedZoneId
+                    name: cloudfrontDistribution.domainName,
+                    zoneId: cloudfrontDistribution.hostedZoneId
                 }
             ]
         });
