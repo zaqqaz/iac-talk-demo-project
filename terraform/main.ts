02342880186b4c6b8793465cdeaca3c30a9911bb
Denis Artyuhovich <i@denis.by>
2021-02-15 15:47:46 +0000

    Add public s3 configuration

--- terraform/main.ts
+++ terraform/main.ts
@@ -1,10 +1,27 @@
 import { Construct } from 'constructs';
 import { App, TerraformStack } from 'cdktf';
+import { AwsProvider, S3Bucket } from "./.gen/providers/aws";
 
 class MyStack extends TerraformStack {
     constructor(scope: Construct, name: string) {
         super(scope, name);
 
+        const bucketName = 'iac-talk-demo-project.ux.by'
+
+        const provider = new AwsProvider(this, 'default', {
+            region: 'us-east-1',
+        });
+
+        new S3Bucket(this, 'resource', {
+            bucket: bucketName,
+            provider,
+            acl: "public-read",
+            website: [
+                {
+                    indexDocument: 'index.html'
+                }
+            ],
+        });
     }
 }
 
